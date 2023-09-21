74bfb7bb5600dda0efaf0a1789a91b006f66f1ec
daoudahmad <daoud@hotmail.dk>
2023-09-21 13:11:49 +0200

    Initial commit

---
+++ Book.cs
@@ -0,0 +1,20 @@
+﻿namespace obg_opgave;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int  Price { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Id},{Title},{Price}";
+    }
+
+    public void ValidateTitle()
+    {
+
+    }
+
+}
+
